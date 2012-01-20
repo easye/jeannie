467440a3e20eb6052327ff92f9f341d8716112e7
Mark <evenson@panix.com>
2012-01-20 16:11:58 +0100

    Initial download and local cache of Jena jar artifacts via Maven.

---
+++ jeannie.asd
@@ -0,0 +1,16 @@
+;;; -*- Mode: LISP; Syntax: COMMON-LISP -*-
+(require 'asdf)
+(in-package :asdf)
+(defsystem :jeannie 
+  :version "0.1"
+  :documentation "A wrapping of Jena for Armed Bear Common Lisp."
+  :components
+  ((:module jena :components
+            ((:mvn "com.hp.hpl.jena/arq" :version "2.8.5")))
+   (:module src :pathname "" :depends-on (:jena) :components 
+            ((:file "packages")))))
+  
+
+
+   
+            
---
+++ packages.lisp
@@ -0,0 +1,15 @@
+#-abcl (error "Sorry, but this will only work with Armed Bear Common Lisp.")
+#-abcl (warn "http://common-lisp.net/project/armedbear/")
+
+(defpackage :jeannie
+  (:use :cl :abcl-asdf)
+  (:documentation "http://en.wikipedia.org/wiki/I_Dream_of_Jeannie")
+  (:nicknames :jena)
+  (:export #:index))
+
+(in-package :jeannie)
+
+(defun index () 
+  "Return the index of locally cached Jena jars that will be used"
+  ;;; TODO use these libraries in the ASDF definition.
+  (abcl-asdf:resolve-dependencies "org.apache.jena" "jena-arq"))
