17cb5c7327615130111413975f870b17e0b7d765
Mark <evenson@panix.com>
2012-02-01 10:28:40 +0100

    bugfix: restore compilation.
    Rename READ to READ-RDF.

--- src/jena.lisp
+++ src/jena.lisp
@@ -1,11 +1,11 @@
 (in-package :jeannie)
 
 ;;; FIXME need a test
-(defun read (&key (file "index.rdf"))
+(defun read-rdf (&key (file "index.rdf"))
   (with-open-stream (s file :direction :input)
     (let* ((lang "N3")
            (base "https://jena.not.org/index#rdf")
            (reader (#"getReader" 'RDFReaderF lang))
-           (model (jss:jnew 'Model))
+           (model (jss:new 'Model))
            (input-stream (jstream s)))
       (#"read" input-stream base lang))))
