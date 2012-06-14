f6bd86c05c038e2b8aa5b6d812e0fe59bc5c9c4f
Mark <evenson@panix.com>
2012-06-15 01:17:46 +0200

    READ-RDF takes a single required argument as to the source.
    Refactor out the &optional &key nonsense for READ-RDF before anyone actually uses this.

--- src/jena.lisp
+++ src/jena.lisp
@@ -2,11 +2,16 @@
 
 (defparameter *model* nil)
 
-;;; FIXME need a test
-(defun read-rdf (&optional &key (file #p"index.rdf"))
+;;; FIXME define as generic function
+(defun read-rdf (filename)
+  "Reads the contents of FILENAME as n3 triples. 
+
+As a side-effect, the special variable JEANNIE:*MODEL* is set to the
+result of this call."
+
   (unless *model*
     (setf *model* (#"createDefaultModel" 'ModelFactory)))
-  (with-open-file (s file :direction :input)
+  (with-open-file (s filename :direction :input)
     (let* ((lang "N3")
            (base "https://jena.not.org/index#rdf")
 
--- test/tests.lisp
+++ test/tests.lisp
@@ -1,7 +1,7 @@
 (in-package :jeannie)
 (rt:deftest jeannie.read-rdf.1
     (#"toString"
-     (read-rdf :file (asdf:system-relative-pathname :jeannie "test/tests.n3")))
+     (read-rdf (asdf:system-relative-pathname :jeannie "test/tests.n3")))
   t)
 
     
