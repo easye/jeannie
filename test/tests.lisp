0f6382ab9d994ae7ebe22e40bd1794abe6403e9a
Mark <evenson@panix.com>
2012-06-15 02:40:42 +0200

    test/jeannie.model.1:  test deserializing model.

--- test/tests.lisp
+++ test/tests.lisp
@@ -4,4 +4,9 @@
      (read-rdf (asdf:system-relative-pathname :jeannie "test/tests.n3")))
   t)
 
+;;; XXX needs to have run JEANNIE.READ-RDF.1 to initialize *MODEL*
+(rt:deftest jeannie.model.1
+    (mapcar #"toString" (loop :with iterator = (#"listObjects" *model*) :while (#"hasNext" iterator) :collect (#"next" iterator)))
+  t)
+
     
