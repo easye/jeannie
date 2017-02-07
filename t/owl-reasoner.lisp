8fd61bc8ef632b6514a939de8f8b9fc546da3a57
Mark Evenson <evenson.not.org@gmail.com>
2017-02-07 16:53:13 +0000

    Explicitly use external package system for API we are testing

--- t/owl-reasoner.lisp
+++ t/owl-reasoner.lisp
@@ -2,18 +2,18 @@
 
 (plan 4)
 
-(ok (jeannie::get-reasoner :type "OWL"))
+(ok (jeannie:get-reasoner :type "OWL"))
 
 (let ((model (read-rdf (asdf:system-relative-pathname :jeannie "t/eg/people.n3"))))
   (let ((inferred-model (jeannie:inferred-model model)))
-    (diag "Testing whether in memory model was createdâ€¦")
+    (diag "Testing whether in memory model was created…")
     (ok model)
-    (diag "Testing that an inferred model could be derivedâ€¦")
+    (diag "Testing that an inferred model could be derived…")
     (ok inferred-model)
-    (diag "Iterating statements from inferred modelâ€¦")
+    (diag "Iterating statements from inferred model…")
     (ok
      (let ((statements
-            (list-statements inferred-model :subject "http://example.org/easye/")))
+            (jeannie:list-statements inferred-model :subject "http://example.org/easye/")))
        (and
         statements
         (> (length statements) 0))))))
