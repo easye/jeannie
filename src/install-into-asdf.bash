e0c5fef534526709e9fc69a6ed1283b22b02fa1d
Mark <evenson.not.org@gmail.com>
2017-11-27 12:02:45 +0100

    Devops installation integration via self-contained bash scripting

---
+++ src/install-into-asdf.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+dest_d="$HOME/.config/common-lisp/source-registry.conf.d/"
+mkdir -p ${dest_d}
+cp ${IDR}/jeannie.conf "${dest_d}"
+
